918471e5b9d90a128ac23470631b3241488f071a
fancymsw <a2638876607@gmail.com>
2022-04-08 16:37:31 +0800

    vue-demo

---
+++ hmhr/src/store/modules/user.js
@@ -0,0 +1,22 @@
+
+const getDefaultState = () => {
+
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  }
+}
+
+const actions = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
